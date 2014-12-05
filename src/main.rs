9880fde8e3d4a308cb8d4c2fb58a6e1c1ac7294d
Vincent Nys <vincentnys@gmail.com>
2014-12-05 22:24:30 +0100

    Don't understand this error message about not being able to index a value.

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,24 @@ struct Location {
 // "go + direction": move to the area in that direction
 // "use + item (+ object)": maybe remove the item, invoke a (targeted?) effect
 
-// one option: just match the enum value, parse the rest and act based on a match
-enum Action {
-    Look,
-    Take,
-    Go,
-    Use,
+// alternatively, instantiate actions at runtime
+// write a struct for each one with arity and synonyms
+// problem: each one is associated with a function of some sort
+// do not want to go adding these at runtime if it can be avoided
+
+fn parse(command: String) {
+    let tokens = command.split(' ');
+    match tokens[0] {
+        "look" => println!("You look around."),
+        "take" => println!("You take something."),
+        "go" => println!("You go somewhere."),
+        "use" => println!("You use something."),
+        _ => println!("Unknown command!"),
+    }
+    // look should just check for arity 0 invoke an associated function
+    // take should parse rest of the command, see if there is an item with a name equal to the rest of the command
+    // go should parse exactly one more token and go in that direction... how?
+    // use should parse either one or two tokens and act accordingly
 }
 
 fn main() {
