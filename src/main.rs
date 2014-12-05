2ab01078a5e69d4b9a69075be417665129e807f4
Vincent Nys <vincentnys@gmail.com>
2014-12-05 09:23:57 +0100

    String type mismatch. Item needs a string reference rather than a copy.

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,8 @@ struct Location {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let item1 = Item("Little key");
+    let item2 = Item("Big key");
+    let hall = Location{ description: "A wide open space. Streams of lava flow on both sides of the entry gate.",
+                         items: vec![item1, item2] };
 }
